a2e1527ece390270e4fb62861e96c25d30c0ae0c
N.O.W <Weicholdo02@gmail.com>
2023-06-06 12:03:39 +0200

    Data Manager maybe working

--- Assets/Scripts/SaveSystem/DataManager.cs
+++ Assets/Scripts/SaveSystem/DataManager.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 
-public class DataManager : MonoBehaviour
+[ExecuteInEditMode]
+public class DataManager : ListFunctionsExtension
 {
     [HideInInspector] public SaveDataContainer SaveDataContainer;
+
+    [SerializeField] private string tagItems;
+    [SerializeField] private string tagRooms;
+    [SerializeField] private List<InventorySaveEnabler> inventorySaveAssing;
+    [SerializeField] private List<RoomIDAssing> roomIDAssings;
 
     public void GetData()
     {
@@ -25,10 +32,41 @@ public class DataManager : MonoBehaviour
         {
             SaveDataContainer.InteractionsPathsSave.Add(ItemInteractionBrain.InteractionsStatic[i].Paths);
         }
-        
-        
-        SaveDataContainer.InventorySave = PlayerBrain.Inventory;
-        SaveDataContainer.RoomSave = RoomOrganizer.RoomsStatic;
+
+        //SaveDataContainer.InventorySave = PlayerBrain.Inventory;
+        for (int i = 0; i < PlayerBrain.Inventory.Items.Length; i++)
+        {
+            if (PlayerBrain.Inventory.Items[i] != null)
+            {
+                continue;
+            }
+            for (int j = 0; j < inventorySaveAssing.Count; j++)
+            {
+                if (PlayerBrain.Inventory.Items[i].ItemGO == inventorySaveAssing[j].Item)
+                {
+                    SaveDataContainer.InventorySaveIDSave.Add(inventorySaveAssing[j].SaveID);
+                }
+            }
+        }
+
+        //SaveDataContainer.RoomSave = RoomOrganizer.RoomsStatic;
+        for (int i = 0; i < RoomOrganizer.RoomsStatic.Length; i++)
+        {
+            for (int j = 0; j < roomIDAssings.Count; j++)
+            {
+                if (RoomOrganizer.RoomsStatic[i].RoomGO == roomIDAssings[j].Room)
+                {
+                    RoomSaveEnabler tmpRoomSaveEnabler = new()
+                    {
+                        RoomSaveID = roomIDAssings[j].SaveID,
+                        Locked = RoomOrganizer.RoomsStatic[i].IsLocked
+                    };
+                    SaveDataContainer.RoomSaveEnablerSave.Add(tmpRoomSaveEnabler);
+                    break;
+                }
+            }
+        }
+
         SaveDataContainer.StylesSave = GameBrainScript.Styles.ToArray();
 
         Debug.Log("LoadDatasuc!");
@@ -50,11 +88,77 @@ public class DataManager : MonoBehaviour
             ItemInteractionBrain.InteractionsStatic[i].Paths = SaveDataContainer.InteractionsPathsSave[i];
         }
 
-        PlayerBrain.Inventory = SaveDataContainer.InventorySave;
-        RoomOrganizer.RoomsStatic = SaveDataContainer.RoomSave;
+        //PlayerBrain.Inventory = SaveDataContainer.InventorySave;
+        for (int i = 0; i < PlayerBrain.Inventory.Items.Length; i++)
+        {
+            PlayerBrain.Inventory.Items[i].ItemGO = null;
+            PlayerBrain.Inventory.Items[i].ItemSelected = false;
+        }
+        for (int i = 0; i < SaveDataContainer.InventorySaveIDSave.Count; i++)
+        {
+            for (int j = 0; j < inventorySaveAssing.Count; j++)
+            {
+                if (SaveDataContainer.InventorySaveIDSave[i] == inventorySaveAssing[j].SaveID)
+                {
+                    PlayerBrain.Inventory.Items[i].ItemGO = inventorySaveAssing[j].Item;
+                }
+            }
+        }
+
+
+        //RoomOrganizer.RoomsStatic = SaveDataContainer.RoomSave;
+        for (int i = 0; i < SaveDataContainer.RoomSaveEnablerSave.Count; i++)
+        {
+            for (int j = 0; j < roomIDAssings.Count; j++)
+            {
+                if (SaveDataContainer.RoomSaveEnablerSave[i].RoomSaveID != roomIDAssings[j].SaveID)
+                {
+                    continue;
+                }
+                for (int k = 0; k < RoomOrganizer.RoomsStatic.Length; k++)
+                {
+                    if (RoomOrganizer.RoomsStatic[k].RoomGO == roomIDAssings[j].Room)
+                    {
+                        RoomOrganizer.RoomsStatic[k].IsLocked = SaveDataContainer.RoomSaveEnablerSave[i].Locked;
+                        break;
+                    }
+                }
+            }
+        }
+
         GameBrainScript.Styles = SaveDataContainer.StylesSave.ToList();
 
         Debug.Log("SyncDatasuc!");
+    }
+
+    private void UpdateItemSaveIDList()
+    {
+        GameObject[] tmpItemGOs = GameObject.FindGameObjectsWithTag(tagItems);
+        ListCompare(inventorySaveAssing, tmpItemGOs.ToList(), () => new InventorySaveEnabler());
+        for (int i = 0; i < inventorySaveAssing.Count; i++)
+        {
+            inventorySaveAssing[i].SaveID = i;
+        }
+    }
+
+    private void UpdateRoomIDAssing()
+    {
+        GameObject[] tmpRoomGOs = GameObject.FindGameObjectsWithTag(tagRooms);
+        ListCompare(roomIDAssings, tmpRoomGOs.ToList(), () => new RoomIDAssing());
+        for (int i = 0; i < roomIDAssings.Count; i++)
+        {
+            roomIDAssings[i].SaveID = i;
+        }
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor && Application.isPlaying)
+        {
+            return;
+        }
+        UpdateItemSaveIDList();
+        UpdateRoomIDAssing();
     }
 }
 
@@ -66,9 +170,46 @@ public class SaveDataContainer
     
     //public Interaction[] InteractionsSave;
     public List<Paths[]> InteractionsPathsSave;
-    
-    public Inventory InventorySave;
-    public Room[] RoomSave;
+
+    //public Inventory InventorySave;
+    public List<int> InventorySaveIDSave;
+
+    //public Room[] RoomSave;
+    public List<RoomSaveEnabler> RoomSaveEnablerSave;
+
     public GameBrainScript.Style[] StylesSave;
 
+}
+
+[Serializable]
+public class InventorySaveEnabler : Translate
+{
+    public GameObject Item;
+    public int SaveID;
+
+    public GameObject GOTranslate
+    {
+        get { return Item; }
+        set { Item = value; }
+    }
+}
+
+[Serializable]
+public class RoomIDAssing : Translate
+{
+    public GameObject Room;
+    public int SaveID;
+
+    public GameObject GOTranslate
+    {
+        get { return Room; }
+        set { Room = value; }
+    }
+}
+
+[Serializable]
+public class RoomSaveEnabler
+{
+    public int RoomSaveID;
+    public bool Locked;
 }
--- Assets/Scripts/SaveSystem/SaveFunctions.cs
+++ Assets/Scripts/SaveSystem/SaveFunctions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System;
 
 public class SaveFunctions : MonoBehaviour
 {
